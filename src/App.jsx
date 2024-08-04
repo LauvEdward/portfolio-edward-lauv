cfa9f79c8690383d23daec8074ede924da3a2fdf
Lauv Edward <edwardlauv@gmail.com>
2024-08-04 10:47:32 +0700

    change logo make hero ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { NavBar } from './components/NavBar'
+import { Hero } from './components/Hero'
 
 const App = () => {
   return (
@@ -6,7 +7,8 @@ const App = () => {
       <div className="flixed top-0 -z-10 h-full w-full"></div>
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container mx-auto px-8">
-      <NavBar></NavBar>
+      <NavBar/>
+      <Hero/>
       </div>
     </div>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div>
+        <h1 className="pb-16 tex">Edward Lauv</h1></div>
+  )
+}
